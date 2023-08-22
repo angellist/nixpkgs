85a2dbe9f1bbbb4deb2936ef78c4b821ad1fb040
korbin <k1@k1.io>
2023-08-22 15:16:35 -0600

    update mydumper, add darwin support

--- pkgs/tools/backup/mydumper/default.nix
+++ pkgs/tools/backup/mydumper/default.nix
@@ -1,30 +1,27 @@
-{ lib, stdenv, fetchFromGitHub
-, cmake, pkg-config, sphinx
-, glib , pcre
-, libmysqlclient, libressl
-, zlib, zstd
-}:
+{ lib, stdenv, fetchFromGitHub, cmake, pkg-config, sphinx, glib, pcre
+, libmysqlclient, libressl, zlib, zstd }:
 
 stdenv.mkDerivation rec {
   pname = "mydumper";
-  version = "0.14.3-1";
+  version = "0.15.2-4";
 
   src = fetchFromGitHub {
-    owner  = "mydumper";
+    owner = "mydumper";
     repo = "mydumper";
     rev = "refs/tags/v${version}";
-    hash = "sha256-qyJGnrBOElQ3s2VoOWfW1luacd33haanmzKidMBgCpc=";
+    hash = "sha256-j2qUvjVl7XXOM3fnicjkHkW9jjyEV0v6FClfgqDemLA=";
   };
+
+  patches = lib.optionals stdenv.isDarwin [ ./darwin.patch ];
 
   outputs = [ "out" "doc" "man" ];
 
   nativeBuildInputs = [ cmake pkg-config sphinx ];
 
-  buildInputs = [
-    glib pcre
-    libmysqlclient libressl
-    zlib zstd
-  ];
+  buildInputs = [ glib pcre libmysqlclient libressl zlib zstd ];
+
+  env.NIX_CFLAGS_COMPILE =
+    toString (lib.optionals stdenv.isDarwin [ "-Wno-sometimes-uninitialized" ]);
 
   cmakeFlags = [
     "-DCMAKE_SKIP_BUILD_RPATH=ON"
@@ -37,7 +34,7 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/maxbube/mydumper";
     changelog = "https://github.com/mydumper/mydumper/releases/tag/v${version}";
     license = licenses.gpl3Plus;
-    platforms = platforms.linux;
+    platforms = platforms.linux ++ platforms.darwin;
     maintainers = with maintainers; [ izorkin ];
   };
 }
